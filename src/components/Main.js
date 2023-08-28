3a0c2852031a7f7fc9f093e8ccbf502b6aca3f80
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-08-27 21:53:36 -0400

    fetch added to fakeDataSet

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,22 @@
+import {useState, useEffect} from 'react'
 import Card from "./Card.js"
 
-export default function Home(){
+export default function Main(){
+    const [facebookAds, setFacebookAds] = useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:3000/fakeDataSet')
+            .then(r => {
+                if(r.ok) {
+                    r.json().then(data=>{
+                        setFacebookAds(data)
+                    })
+                } else {
+                    console.log(r.status)
+                }
+            })
+    },[])
+
     return (
         <div>
             <h1>Main</h1>

68a863aa27cb8b975f9a119ca611a4ae3cc5e5a9
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-08-28 23:35:55 -0400

    search by campaign namefeature added

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import Card from "./Card.js"
 
 export default function Main(){
     const [allAds, setAllAds] = useState([])
+    const [searchedCampaign, setSearchedCampaign] = useState('')
 
     useEffect(()=>{
         fetch('http://localhost:3000/fakeDataSet')
@@ -17,7 +18,16 @@ export default function Main(){
             })
     },[])
 
-    const cardComponents = allAds.map((ad)=>{
+    //search by campaign name
+    const handleSearch = e => setSearchedCampaign(e.target.value)
+    const searchByCampaign = allAds.filter(ad => {
+        const campaign = ad.campaign_name ?? ad.campaign ?? ad.utm_campaign
+
+        return(campaign.toLowerCase().includes(searchedCampaign.toLowerCase()))
+    })
+
+    //pass props to Card component
+    const cardComponents = searchByCampaign.map((ad)=>{
         const campaign = ad.campaign_name ?? ad.campaign ?? ad.utm_campaign
         const adset = ad.media_buy_name ?? ad.ad_group ?? ad.ad_squad_name ?? ad.utm_medium 
         const creative = ad.ad_name ?? ad.image_name ?? ad.creative_name ?? ad.utm_content
@@ -30,9 +40,11 @@ export default function Main(){
     })
     
 
+
     return (
         <div>
             <h1>Main</h1>
+            <input onChange={handleSearch}/>
             {cardComponents}
         </div>
     )
